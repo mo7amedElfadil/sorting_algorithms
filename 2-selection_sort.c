f854ba489e5d55fa18d71167fc597206e058b105
mo7amedElfadil <mohamedelfadil1994@gmail.com>
2023-12-14 21:21:10 +0200

    2-selection_sort.c

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,4 +1,5 @@
 #include "sort.h"
+#include <stddef.h>
 /**
  * selection_sort - Sorts an array of integers in ascending order using
  *		The selection sort algorithm
@@ -8,6 +9,20 @@
  */
 void selection_sort(int *array, size_t size)
 {
+	size_t i, j, min;
 
-
+	for (i = 0; i < size; i++)
+	{
+		min = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min])
+				min = j;
+		}
+		if (min != i)
+		{
+			swap_values(&array[i], &array[min]);
+			print_array(array, size);
+		}
+	}
 }
